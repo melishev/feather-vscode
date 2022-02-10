5d94712cf3bb808c499c81b0907f11def12f614b
Matvey Melishev <58311421+melishev@users.noreply.github.com>
2022-02-10 23:46:33 +0300

    ci: update ci, add extension to MarketPlace

---
+++ .releaserc.js
@@ -0,0 +1,22 @@
+module.exports = {
+  plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
+      "@semantic-release/changelog",
+      [
+        "semantic-release-vsce",
+        {
+          packageVsix: true,
+        },
+      ],
+    '@semantic-release/npm',
+    [
+      "@semantic-release/git",
+      {
+        assets: ["CHANGELOG.md"]
+      }
+    ],
+    '@semantic-release/github',
+  ],
+  preset: "angular"
+}
